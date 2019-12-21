4b039d5cd027de31e21168d7ff1e3434b3518f20
Gregory Maksyuk <gregory.maksyuk@gmail.com>
2019-12-21 20:36:03 +0200

    Initial Commit

---
+++ MacPawFileReactor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacPawFileReactor
+//
+//  Created by Gregory Maksyuk on 12/21/19.
+//  Copyright © 2019 Gregory Maksyuk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
